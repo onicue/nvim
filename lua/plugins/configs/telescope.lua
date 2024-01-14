2838ca23ef56a4c5a76484b03a112fc0866c800a
onicue <151780948+onicue@users.noreply.github.com>
2024-01-14 16:19:40 +0100

    Refactored and enhanced Telescope configuration and keybind

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -5,13 +5,62 @@ return {
   config = function ()
     require('telescope').setup {
       defaults = {
-        -- sorting_strategy = "actions",
-        layout_strategy = "horizontal",
-        -- layout_config = { prompt_position = "true" },
-        border = false,
+        prompt_prefix = " ",
+        show_line = false,
+        selection_caret = " ",
+        entry_prefix = "  ",
+        initial_mode = "insert",
+        prompt_title = false,
+        results_title = false,
+        preview_title = false,
+        selection_strategy = "reset",
+        sorting_strategy = "descending",
+        layout_strategy = "vertical",
+        layout_config = {
+          horizontal = { mirror = false },
+          vertical = { mirror = true },
+        },
+        file_ignore_patterns = {".git/*", ".cache", ".ssh"},
+        winblend = 5,
+        borderchars = {
+        preview = { " ", " ", " ", " ", " ", " ", " ", " " },
+        prompt = { " ", " ", " ", " ", " ", " ", " ", " " },
+        results = { " ", " ", " ", " ", " ", " ", " ", " " },
+        },
+
+        color_devicons = true,
+        use_less = true,
+        path_display = {},
+
+        set_env = { ["COLORTERM"] = "truecolor" },
+
+        mappings = {
+          i = {
+            ["<esc>"] = require('telescope.actions').close,
+            ["<C-j>"] = require('telescope.actions').move_selection_previous,
+            ["<C-k>"] = require('telescope.actions').move_selection_next,
+            ["<C-n>"] = false,
+            ["<C-p>"] = false,
+          },
+        }
+
       },
-      extensions = {},
-      file_browser = {},
-    }
-  end
+      picker = {
+        find_files = {
+        hidden = true,
+        prompt_title = false,
+        previewer = false,
+        results_title = false,
+        preview_title = false,
+        find_command = { "fd", "--type", "f", "--strip-cwd-prefix" },
+      },
+    }}
+  end,
+  keys = {
+    {"<leader>ff", ":Telescope find_files<cr>",{silent = true, noremap = true}},
+    {'<leader>fg', function()
+    require('telescope.builtin').grep_string({ search = vim.fn.input("Grep > ") })
+    end},
+    {"<leader>fb", ":Telescope buffers<cr>", {silent = true, noremap = true}}
+  }
 }
