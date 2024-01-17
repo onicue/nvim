755332b118436eb858b51eba9c9bc3824c001e5b
onicue <151780948+onicue@users.noreply.github.com>
2024-01-17 14:07:32 +0100

    changed lualine: deleted options

--- lua/plugins/configs/lualine.lua
+++ lua/plugins/configs/lualine.lua
@@ -3,10 +3,6 @@ return{
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
       theme = "auto",
-      options = {
-        component_separators = { left = '|', right = '|'},
-        section_separators = { left = '', right = ' '},
-      },
       sections = { lualine_a = {
         { 'mode', fmt = function(str) return str:sub(1,1) end } },
       },
